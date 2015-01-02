4049bd00e89ea0eacbee2f6b1673a07d226ee5f0
dala0005 <daniel_larsson@linux.com>
2015-01-02 09:53:25 +0100

    a placeholder for admin login

---
+++ admin/index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css.css">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>
+	</title>
+</head>
+<body>
+	<p>Login</p>
+	<p>Användarnamn: </p><input></input>
+	<p>Lösenord: </p><input></input>
+</body>
+</html>
